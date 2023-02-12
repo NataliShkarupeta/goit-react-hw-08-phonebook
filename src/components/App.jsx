9ba1958bfcf3a05de5c1630c1f20b1ef0c922c94
Nataliia Shkarupeta <natalinardi.kh@gmail.com>
2023-02-12 17:27:54 +0200

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { lazy, React, useEffect } from 'react';
+import { React, useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchRefreshUser } from 'redux/user/userOperations';
@@ -8,11 +8,11 @@ import FrontPage from '../pages/FrontPage/FrontPage';
 import PrivatRoute from './PrivetRoute/PrivatRoute';
 import PublicRoute from './PublicRoute/PublicRoute';
 import Login from 'pages/login/login';
-// import RegisterForm from 'pages/registerForm/registerForm';
-// import PersonalPage from 'pages/personalPage/personalPage';
+import RegisterForm from 'pages/registerForm/registerForm';
+import PersonalPage from 'pages/personalPage/personalPage';
 
-const RegisterForm = lazy(() => import('../pages/registerForm/registerForm'));
-const PersonalPage = lazy(() => import('../pages/personalPage/personalPage'));
+// const RegisterForm = lazy(() => import('../pages/registerForm/registerForm'));
+// const PersonalPage = lazy(() => import('../pages/personalPage/personalPage'));
 
 export const App = () => {
   const dispatch = useDispatch();
@@ -47,60 +47,3 @@ export const App = () => {
   );
 };
 
-// import { lazy, React, useEffect } from 'react';
-// import { Route, Routes } from 'react-router-dom';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { fetchRefreshUser } from 'redux/user/userOperations';
-// import { selectIsRegister } from 'redux/user/userSelectors';
-// import Navigation from './Navigation/Navigation';
-// import FrontPage from '../pages/FrontPage/FrontPage';
-// import PrivatRoute from './PrivetRoute/PrivatRoute';
-// import PublicRoute from './PublicRoute/PublicRoute';
-// import Login from 'pages/login/login';
-
-// const RegisterForm = lazy(() => import('../pages/registerForm/registerForm'));
-// const PersonalPage = lazy(() => import('../pages/personalPage/personalPage'));
-
-// export const App = () => {
-//   const dispatch = useDispatch();
-//   const isRegister = useSelector(selectIsRegister);
-//   useEffect(() => {
-//     dispatch(fetchRefreshUser());
-//   }, [dispatch]);
-
-//   return (
-//     <>
-//       <Routes>
-//         <Route path="/" element={<Navigation />}>
-//           <Route index element={<FrontPage />} />
-//           <Route
-//             path="register"
-//             element={
-//               <PublicRoute>
-//                 <RegisterForm />
-//               </PublicRoute>
-//             }
-//           />
-//           <Route
-//             path="login"
-//             element={
-//               <PublicRoute>
-//                 <Login />
-//               </PublicRoute>
-//             }
-//           />
-
-//           <Route
-//             path="contacts"
-//             element={
-//               <PrivatRoute>
-//                 <PersonalPage />
-//               </PrivatRoute>
-//             }
-//           />
-//           <Route path="*" element={isRegister ? <PersonalPage /> : <Login />} />
-//         </Route>
-//       </Routes>
-//     </>
-//   );
-// };
